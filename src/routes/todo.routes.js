c183ecb300c7139e98b5cd46f05ef25ef7089a9d
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-14 12:07:56 -0700

    create: GET request handler to get all todos on DDBB

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -4,8 +4,23 @@ const { Router } = require('express');
 const routes = Router();
 
 
-routes.get('/api/todos', async (req, res) => {
-    res.send('test exitoso')
+routes.get('/api/v1/todos', async (req, res) => {
+    try{
+        const result = await ToDO.findAll({
+            attributes: [
+                'id',
+                'title',
+                'description',
+                'is_completed',
+                'created_at',
+            ]
+        });
+        res.status(200).send(result)
+    }
+    catch(err){
+        console.error(err);
+        res.send(err);
+    }
 })
 
 module.exports = routes;
