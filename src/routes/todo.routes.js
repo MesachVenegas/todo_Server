d0b79a42668bee46d16b839338f6a98639aa02d4
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-14 19:49:59 -0700

    add: routes to create, delete, update and get all todos or one todo

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -3,9 +3,9 @@ const { Router } = require('express');
 
 const routes = Router();
 
-
+// get all todos on DDBB.
 routes.get('/api/v1/todos', async (req, res) => {
-    try{
+    try {
         const result = await ToDO.findAll({
             attributes: [
                 'id',
@@ -17,10 +17,64 @@ routes.get('/api/v1/todos', async (req, res) => {
         });
         res.status(200).send(result)
     }
-    catch(err){
+    catch (err) {
+        console.error(err);
+        res.send(err);
+    }
+});
+
+// Get todo by pk.
+routes.get('/api/v1/todos/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await ToDO.findByPk(id)
+        res.send(result);
+    }
+    catch (err) {
+        console.error(err);
+        res.send(err);
+    }
+});
+
+//   Create a todo.
+routes.post('/api/v1/todos', async (req, res) => {
+    try {
+        const newToDo = req.body;
+        await ToDO.create(newToDo);
+        res.status(204).send('');
+    }
+    catch (err) {
         console.error(err);
         res.send(err);
     }
 })
+
+// update a todo by pk.
+routes.put('/api/v1/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const result = await ToDO.update(data, {
+            where: {
+                id: id
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.send(error);
+    }
+})
+
+// Delete todo by id/
+routes.delete('/api/v1/todos/:id', async (req, res) =>{
+    try {
+        const { id } = req.params;
+        const result = await ToDO.destroy({where: {id: id}});
+        res.send('');
+    } catch (error) {
+        console.error(error);
+        res.send(error)
+}
+});
 
 module.exports = routes;
